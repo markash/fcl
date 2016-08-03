c8fb32a0e9fa5b388eb78087865cd29fcc1ebe91
Mark Ashworth <mp.ashworth@gmail.com>
2016-08-03 07:43:18 +0200

    Changed to use express

--- app.js
+++ app.js
@@ -1,50 +1,18 @@
-var http = require('http');
-var path = require('path');
-var fs = require('fs');
+var express = require('express');
+var app = express();
 
-var mimeType = function(extension) {
+app.set('port', (process.env.PORT || 5000));
 
-    if (extension == 'html' || extension == 'HTML') {
-        return 'text/html';
-    } else if (extension == 'ico' || extension == 'ICO') {
-        return 'image/x-icon';
-    } 
+app.use(express.static(__dirname + '/public'));
 
-    return 'text/html';
-};
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
-var server = http.createServer(function(request, response) {
-    console.log(request.method + " : " + request.url);
-
-    if (request.method == 'GET') {
-
-        if (request.url == '/') {
-            request.url = '/index.html';
-        }
-
-        var filePath =  path.resolve("./public" + request.url);
-        var extension = path.extname(filePath);
-
-        console.log('Extension : ' + extension);
-
-        fs.exists(filePath, function(exists) {
-            if (exists) {
-
-                response.writeHead(200, {'ContentType': mimeType(extension)});
-                fs.createReadStream(filePath).pipe(response);
-            }
-            else {
-                response.writeHead(404, {'ContentType': 'text/html'});
-                response.end('<html><body>Not found</body></html>')
-            }
-        });
-    }
-    else {
-        response.statusCode = 403;
-        response.end('<html><body>Not supported</body></html>')
-    }
-
-    
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
 
-server.listen(3001);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
